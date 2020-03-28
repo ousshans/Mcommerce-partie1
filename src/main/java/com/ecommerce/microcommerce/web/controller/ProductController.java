ef8f70b52f1122f2be1f77763a262e0b0e2560fc
oussama hansali <ouss.kopp@gmail.com>
2020-03-28 15:41:38 +0100

    Partie 3

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -74,6 +75,7 @@ public class ProductController {
 
         if (productAdded == null)
             return ResponseEntity.noContent().build();
+        if(productAdded.getPrix() == 0) throw new ProduitGratuitException("Se produit est gratuit");
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
